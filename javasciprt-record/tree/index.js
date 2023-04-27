d9702251725b0e77b86fdee7f5af16e7670b196d
xiaolong666666 <long.1585097369@qq.com>
2023-04-27 22:17:01 +0800

    fix: array2tree

--- javasciprt-record/tree/index.js
+++ javasciprt-record/tree/index.js
@@ -33,13 +33,13 @@ const inputTree =
   //     { id: 1, text: 'text1' },
   // ];
 
-// const inputArray = 
-//   [
-//     { id: 1, name: 'i1' },
-//     { id: 2, name: 'i2', parentId: 1 },
-//     { id: 4, name: 'i4', parentId: 3 },
-//     { id: 3, name: 'i3', parentId: 2 },
-//   ]
+const inputArray = 
+  [
+    { id: 1, name: 'i1' },
+    { id: 2, name: 'i2', parentId: 1 },
+    { id: 4, name: 'i4', parentId: 3 },
+    { id: 3, name: 'i3', parentId: 2 },
+  ]
 
 const tree2Array = (tree) => {
   // let result = []
@@ -53,34 +53,46 @@ const tree2Array = (tree) => {
 
   return Array.isArray(tree)
     ? tree.reduce((total, current) =>
-      total.concat(Array.isArray(current.children) ? tree2Array(current.children) : [], delete current.children ? current : current)
+      total.concat(Array.isArray(current.children) ? tree2Array(current.children) : [], delete current.children && current)
       , [])
     : [tree]
 }
 
 const array2tree = (array) => {
-  const parents = array.filter(({ parentId }) => !parentId)
-  const childrens = array.filter(({ parentId }) => !!parentId)
-  const recursive = (parents, childrens) => {
-    parents.forEach(parent => {
-      childrens.forEach(children => {
-        if (parent.id === children.parentId) {
-          recursive([children], childrens)
-          if (parent.children) {
-            parent.children.push(children)
-          } else {
-            parent.children = [children]
-          }
-        }
-      })
-    })
+  // const parents = array.filter(({ parentId }) => !parentId)
+  // const childrens = array.filter(({ parentId }) => !!parentId)
+  // const recursive = (parents, childrens) => {
+  //   parents.forEach(parent => {
+  //     childrens.forEach(children => {
+  //       if (parent.id === children.parentId) {
+  //         recursive([children], childrens)
+  //         if (parent.children) {
+  //           parent.children.push(children)
+  //         } else {
+  //           parent.children = [children]
+  //         }
+  //       }
+  //     })
+  //   })
+  // }
+  // recursive(parents, childrens)
+  // return parents
+
+  const map = new Map()
+  for (let obj of array) map.set(obj.id, obj)
+  const result = []
+  for (let obj of array) {
+    if (map.has(obj.parentId)) {
+      map.get(obj.parentId).children ? map.get(obj.parentId).children.push(obj) : map.get(obj.parentId).children = [obj]
+    } else {
+      result.push(obj)
+    }
   }
-  recursive(parents, childrens)
-  return parents
+  return result
 }
 
-const outputArray = tree2Array(inputTree)
-console.log('outputArray', outputArray)
+// const outputArray = tree2Array(inputTree)
+// console.log('outputArray', outputArray)
 
-// const outputTree = array2tree(inputArray)
-// console.log('outputTree', outputTree)
+const outputTree = array2tree(inputArray)
+console.log('outputTree', outputTree)
