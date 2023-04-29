cbd17a46949cb202c29463c72a09b368fd3e06a4
xiaolong666666 <long.1585097369@qq.com>
2023-04-29 23:26:03 +0800

    fix

--- javasciprt-record/closure/index.js
+++ javasciprt-record/closure/index.js
@@ -4,22 +4,32 @@ const debouncing = (func, delay) => {
     let timer = null
 
     return function () {
-        if (timer !== null) {
-            clearTimeout(timer)
-        }
+        if (timer) clearTimeout(timer)
         timer = setTimeout(func, delay)
     }
 }
 
 // 节流:如果一个函数持续的，频繁地触发，在一定时间间隔内只允许执行一次
 const throttling = (func, delay) => {
-    let prev = Date.now()
+    // let prev = Date.now()
     
+    // return function () {
+    //     let now = Date.now()
+    //     if (now - prev > delay) {
+    //         func()
+    //         prev = now
+    //     }
+    // }
+
+    let flag = true
+
     return function () {
-        let now = Date.now()
-        if (now - prev > delay) {
-            func()
-            prev = now
+        if (flag) {
+            setTimeout(() => {
+                func()
+                flag = true
+            }, delay)
+            flag = false
         }
     }
 }
