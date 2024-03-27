c888b571ed4554faf7b99ac95bb5e1ae6d1cab23
xiaolong666666 <long.1585097369@qq.com>
2024-03-27 21:49:43 +0800

    perf: bind & debounce & throttle

--- javasciprt-record/oneself/Bind.js
+++ javasciprt-record/oneself/Bind.js
@@ -1,86 +1,79 @@
 Function.prototype.myCall = function () {
-    const args = Array.from(arguments)
-    let thisArgs = args.shift() ?? window
-    thisArgs = Object(thisArgs)
+  if (typeof this !== "function") {
+    throw new Error("type error");
+  }
+  const args = Array.from(arguments);
+  let context = args.shift() ?? window;
+  context = Object(context);
 
-    // 防止覆盖掉原有属性
-    const key = Symbol()    
+  // 防止覆盖掉原有属性
+  const key = Symbol();
 
-    // 将this绑定在 __proto__ 上防止在 Function 打印 this 时会出现多余的属性
-    thisArgs.__proto__[key] = this  
-    const result = thisArgs[key](...args)
-    delete thisArgs.__proto__[key]
-    return result
-}
+  // 将this绑定在 __proto__ 上防止在 Function 打印 this 时会出现多余的属性
+  context.__proto__[key] = this;
+  const result = args?.length ? context[key](...args) : context[key]();
+  delete context.__proto__[key];
+
+  return result;
+};
 
 Function.prototype.myApply = function () {
-    let thisArgs = arguments[0] ?? window
-    thisArgs = Object(thisArgs)
-    let args = arguments[1] || []
+  if (typeof this !== "function") {
+    throw new Error("type error");
+  }
+  const args = [...arguments];
+  let context = args.shift() ?? window;
+  context = Object(context);
 
-    // 防止覆盖掉原有属性
-    const key = Symbol()
+  // 防止覆盖掉原有属性
+  const key = Symbol();
 
-    // 将this绑定在 __proto__ 上防止在 Function 打印 this 时会出现多余的属性
-    thisArgs.__proto__[key] = this
-    const result = thisArgs[key](...args)
-    delete thisArgs.__proto__[key]
-    return result
-}
+  // 将this绑定在 __proto__ 上防止在 Function 打印 this 时会出现多余的属性
+  context.__proto__[key] = this;
+  const result = args[1]?.length ? context[key](...args[1]) : context[key]();
+  delete context.__proto__[key];
+
+  return result;
+};
 
 Function.prototype.myBind = function () {
-    let args = [...arguments]
-    let thisArgs = args.shift() || window
-    let thisFunc = this
+  let args = Array.prototype.slice.call(arguments);
+  let context = args.shift() ?? window;
+  context = Object(context);
+  let thisFunc = this;
 
-    // 因为需要支持构造函数，所以不能使用匿名函数
-    let bound = function () {
-        let newArgs = [...args, ...arguments]
-        const isStructureFunc = this instanceof bound
-        // 判断是否是构造函数
-        thisArgs = isStructureFunc ? Object.create(thisFunc.prototype) : thisArgs
-        thisFunc.apply(thisArgs, newArgs)
-        if (isStructureFunc) return thisArgs
-    }
+  // 因为需要支持构造函数，所以不能使用匿名函数
+  let bound = function () {
+    // 判断是否是构造函数
+    const isStructureFunc = this instanceof bound;
+    context = isStructureFunc ? Object.create(thisFunc.prototype) : context;
 
-    // Object.create 拷贝原型对象
-    bound.prototype = Object.create(thisFunc.prototype)
+    const result = thisFunc.myApply(context, [...args, ...arguments]);
+    if (result instanceof Object) return result;
+    if (isStructureFunc) return context;
+  };
 
-    return bound
+  // Object.create 拷贝原型对象
+  bound.prototype = Object.create(thisFunc.prototype);
+
+  return bound;
+};
+
+function A(info = {}) {
+  console.log(this);
+  Object.keys(info).forEach((key) => {
+    this[key] = info[key];
+  });
 }
 
-Function.prototype.newBind = function () {
-    const _this = this
-    const args = Array.prototype.slice.call(arguments)
-    const newThis = args.shift()
-
-    return function () {
-        return _this.newApply(newThis, args)
-    }
-}
-
-Function.prototype.newApply = function (context) {
-    if (typeof this !== 'function') {
-        throw new Error('type error')
-    }
-    context = context || window
-    const key = Symbol()
-    context.__proto__[key] = this
-    const result = arguments[1] ? context.__proto__[key](...arguments[1]) : context.__proto__[key]()
-    delete context.__proto__[key]
-
-    return result    
-}
-
-Function.prototype.newCall = function (context) {
-    if (typeof this !== 'function') {
-        throw new Error('type error')
-    }
-    context = context || window
-    context.fn = this
-    const args = Array.from(arguments).slice(1)
-    const result = arguments[1] ? context.fn(...args) : context.fn()
-    delete context.fn
-
-    return result
-}
+const info = { name: "jarry", age: 18, sex: "gril" };
+const a = new A(info);
+const v = A.bind({ v: "vvv" });
+const v1 = v(info);
+const v2 = new v(info);
+console.log({ v1, v2 });
+console.log("---");
+const vv = A.myBind({ v: "vvv" });
+const vv1 = vv(info);
+const vv2 = new vv(info);
+console.log({ vv1, vv2 });
---
+++ javasciprt-record/oneself/shake.js
@@ -0,0 +1,43 @@
+// 去抖:在一定时间内，如果没有再次触发这个函数，才去真正的去执行函数
+
+const debounce = (func, delay) => {
+  let timer = null;
+
+  return function () {
+    const context = this;
+    const args = [...arguments];
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+};
+
+// 节流:如果一个函数持续的，频繁地触发，在一定时间间隔内只允许执行一次
+const throttle = (func, delay) => {
+  // let prev = Date.now()
+
+  // return function () {
+  //   const context = this;
+  //   const args = [...arguments];
+  //   let now = Date.now();
+  //   if (now - prev > delay) {
+  //     func.apply(context, args);
+  //     prev = now;
+  //   }
+  // };
+
+  let flag = true;
+
+  return function () {
+    const context = this;
+    const args = [...arguments];
+    if (flag) {
+      setTimeout(() => {
+        func.apply(context, args);
+        flag = true;
+      }, delay);
+      flag = false;
+    }
+  };
+};
